7be580a6c0963e0e4bd8063e1827818c38ec2bad
yangkaiyi <yangkaiyi@kinyi.cn>
2018-05-28 11:09:48 +0800

    不小心删了登录后个人意向

--- pages/welcome-intention/welcome-intention.js
+++ pages/welcome-intention/welcome-intention.js
@@ -1,66 +1,35 @@
-// pages/welcome-intention/welcome-intention.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    city:'上海',
+    tipArr:['产品','设计师','产品','程序员','产品','销售专员',],
+    canTap:false,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  },
   onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  next(){
+    if(this.data.canTap){
+      console.log(111)
+      wx.switchTab({
+        url: '/pages/mine/personal-mine/personal-mine'
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  changeActiveTip(e){
+    this.setData({
+      "activeTip":e.currentTarget.dataset.index,
+      "canTap":true
+    })
   },
+  hasContent(e){
+    if (e.detail.value.trim().length>0) {
+      this.setData({"canTap":true})
+    }else{
+      this.setData({"canTap":false})
+    }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
