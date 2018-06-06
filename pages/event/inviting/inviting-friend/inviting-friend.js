b0f02d4d62c0ef6869d641c00c9fefe502cf02b1
wang <2021436652@qq.com>
2018-06-06 13:28:19 +0800

    分享完成 Signed-off-by: wang <2021436652@qq.com>

--- pages/event/inviting/inviting-friend/inviting-friend.js
+++ pages/event/inviting/inviting-friend/inviting-friend.js
@@ -5,6 +5,25 @@ Page({
     inviteNumber:0,
     recommNumber:0,
   },
+  invite() {
+    console.log("b")
+    wx.showShareMenu({
+      withShareTicket: true,
+      success() {
+        console.log("a")
+      }
+    })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '蜗牛职信',
+      path: '/pages/job/personal-jobindex/personal-jobindex?id=' + wx.getStorageSync('sessionId') + ''
+    }
+  },
   onLoad: function (options) {
     let _this = this;
     wx.request({ //我的邀请
